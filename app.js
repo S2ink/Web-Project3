fd3c66847b612af2f2459b4593ae142dbb9976cc
Samuel Richter <636654@moundsviewschools.org>
2022-12-12 14:09:58 -0600

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("frame").getContext('2d');
+let width = 500;
+let height = 500;
+let wscale = 2;
+let hscale = 2;
+
+canvas.fillStyle = 'black';
+canvas.fillRect(0, 0, width * wscale, height * hscale);
+canvas.fillText(0, 0, 5, "jfkds")
