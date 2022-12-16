de60ea468d05a9495ac4c99367a8b85f9a9acd7c
Sam Richter <2023richs54@moundsviewschools.org>
2022-12-16 14:09:45 -0600

    canvas fill

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 let canvas = document.getElementById("frame").getContext('2d');
 let width = 500;
 let height = 500;
-let wscale = 2;
-let hscale = 2;
+let wscale = 0.5;
+let hscale = 0.5;
 
 canvas.fillStyle = 'black';
 canvas.fillRect(0, 0, width * wscale, height * hscale);
+
+function paint() {
+	for(let y = 0; y < height / hscale; y++) {
+		for(let x = 0; x < width / wscale ; x++) {
+			
+			let clr;
+			// get color from scene
+			clr = new CCT.Vector3(0, x, y);
+			canvas.fillStyle = `rgb(${clr.x},${clr.y},${clr.z})`;
+			canvas.fillRect(x * wscale, y * hscale, wscale, hscale);
+			
+		}
+	}
+}
+let start = Date.now();
+paint();
+let end = Date.now();
+console.log(end - start);
